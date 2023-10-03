cde7da87fcf58bada82d4552f2afddbd4a84b97d
idk <145403796+itsmrx1234@users.noreply.github.com>
2023-10-03 07:14:26 +0530

    Time: 0 ms (100%), Space: 7.7 MB (7.1%) - LeetHub

--- 1635-number-of-good-pairs/1635-number-of-good-pairs.cpp
+++ 1635-number-of-good-pairs/1635-number-of-good-pairs.cpp
@@ -3,7 +3,7 @@ public:
     int numIdenticalPairs(vector<int>& nums) {
         unordered_map<int,int>mp;
         int ans=0;
-        for(auto &it:nums)
+        for(auto it:nums)
         ans+= mp[it]++;
         return ans;
     }
