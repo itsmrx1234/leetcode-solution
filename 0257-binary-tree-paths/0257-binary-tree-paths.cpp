41400dc0d44800eb4cc7f04290300a4fe785f594
idk <145403796+itsmrx1234@users.noreply.github.com>
2023-10-04 02:28:00 +0530

    Time: 3 ms (64.14%), Space: 14.2 MB (31.69%) - LeetHub

--- 0257-binary-tree-paths/0257-binary-tree-paths.cpp
+++ 0257-binary-tree-paths/0257-binary-tree-paths.cpp
@@ -1,36 +1,26 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
-    vector<string>ans;
-    void generate(TreeNode* root,string s){
-        if(!root)
-        return ;
-        if(!root->left&&!root->right){
-            s+=to_string(root->val);
-            ans.push_back(s);
-            return ;
-        }
-        s+=to_string(root->val);
-        s+="->";
-        generate(root->left,s);
-        generate(root->right,s);
-        s.pop_back();
-        s.pop_back();
-        s+="->";
-    }
     vector<string> binaryTreePaths(TreeNode* root) {
-       string s="";
-       generate(root,s);
-       return ans;
+        vector<string> ans;
+        generatePaths(root, "", ans);
+        return ans;
+    }
+    
+    void generatePaths(TreeNode* node, string path, vector<string>& ans) {
+        if (!node) return;
+        
+        if (path.empty()) {
+            path = to_string(node->val);
+        } else {
+            path += "->" + to_string(node->val);
+        }
+        
+        if (!node->left && !node->right) {
+            ans.push_back(path);
+            return;
+        }
+        
+        generatePaths(node->left, path, ans);
+        generatePaths(node->right, path, ans);
     }
 };
