4d1b770d2cef04358376e67badcf5fadb1a56f5e
idk <145403796+itsmrx1234@users.noreply.github.com>
2023-10-15 18:18:42 +0530

    Time: 16 ms (99.34%), Space: 21.1 MB (36.37%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& st) {
+        unordered_map<string,vector<string>>mp;
+        for(auto it:st){
+            string temp=it;
+            sort(it.begin(),it.end());
+            mp[it].push_back(temp);
+        }
+        vector<vector<string>>ans;
+        for(auto it:mp){
+            ans.push_back(it.second);
+        }
+        return ans;
+    }
+};
