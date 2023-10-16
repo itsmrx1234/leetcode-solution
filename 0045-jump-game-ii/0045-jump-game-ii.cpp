a79d20031966650c290956956584a4fca4c46257
idk <145403796+itsmrx1234@users.noreply.github.com>
2023-10-16 05:46:09 +0530

    Time: 345 ms (20.77%), Space: 18.2 MB (8.36%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.cpp
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -1,14 +1,12 @@
 class Solution {
 public:
     long long generate(vector<int>& nums, int n, int curr, vector<int>& dp) {
-        if (curr == n)
+        if (curr >= n)
             return 0;
         if (dp[curr] != -1)
             return dp[curr];
         long long temp = INT_MAX;
         for (int i = 1; i <= nums[curr]; i++) {
-            if (i + curr > n)
-                break;
             temp = min(temp, 1 + generate(nums, n, curr + i, dp));
         }
         dp[curr] = temp;
