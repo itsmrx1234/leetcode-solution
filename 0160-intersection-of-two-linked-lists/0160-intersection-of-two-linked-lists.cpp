66414e29bcc1b29f2d3addec7cad97ff0a64e5cf
idk <145403796+itsmrx1234@users.noreply.github.com>
2023-10-17 06:34:14 +0530

    Time: 274 ms (8.65%), Space: 14.8 MB (49.56%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
@@ -6,10 +6,10 @@ public:
 
         while (p != q) {
             // If p reaches the end of list A, move it to the head of list A
-            p = (p == nullptr) ? headB : p->next;
+            p = (p == nullptr) ? headA : p->next;
 
             // If q reaches the end of list B, move it to the head of list B
-            q = (q == nullptr) ? headA : q->next;
+            q = (q == nullptr) ? headB : q->next;
         }
 
         return p;
