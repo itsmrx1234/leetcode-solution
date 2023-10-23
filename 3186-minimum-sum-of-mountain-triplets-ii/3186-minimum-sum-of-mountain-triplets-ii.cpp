948339d21d599251d14c56fab6b2838784ea7d16
idk <145403796+itsmrx1234@users.noreply.github.com>
2023-10-23 19:03:57 +0530

    Time: 121 ms (100%), Space: 80.7 MB (100%) - LeetHub

--- 3186-minimum-sum-of-mountain-triplets-ii/3186-minimum-sum-of-mountain-triplets-ii.cpp
+++ 3186-minimum-sum-of-mountain-triplets-ii/3186-minimum-sum-of-mountain-triplets-ii.cpp
@@ -1,24 +1,20 @@
 class Solution {
 public:
     int minimumSum(vector<int>& nums) {
-        int n = nums.size(),minimumSum = INT_MAX;
-        vector<int> prefixMin(n, 1e7), postfixMin(n, 1e8);
-
-        prefixMin[0] = nums[0];
-
-        for (int i = 1; i < n; i++) {
-            prefixMin[i] = min(prefixMin[i - 1], nums[i]);
+        int n = nums.size(),ans = INT_MAX;
+        vector<int>prefix(n,1e7),postfix(n,1e8);
+        prefix[0]=nums[0];
+        postfix[n-1]=nums[n-1];
+        for(int i=1;i<n;i++){
+            prefix[i]=min(prefix[i-1],nums[i]);
         }
-
-        postfixMin[n - 1] = nums[n - 1];
-        for (int i = n - 2; i >= 0; i--) {
-            postfixMin[i] = min(postfixMin[i + 1], nums[i]);
+        for(int i=n-2;i>=0;i--){
+              postfix[i] = min(postfix[i + 1], nums[i]);
         }
-
-        for (int i = 1; i < n - 1; i++) 
-            if (prefixMin[i - 1] < nums[i] && postfixMin[i + 1] < nums[i])
-                minimumSum = min(minimumSum, prefixMin[i - 1] + postfixMin[i + 1] + nums[i]);
-        
-        return minimumSum == INT_MAX ? -1 : minimumSum;
+        for(int i=1;i<n-1;i++){
+            if (prefix[i - 1] < nums[i] && postfix[i + 1] < nums[i])
+            ans=min(ans,nums[i]+prefix[i-1]+postfix[i+1]);
+        }
+        return ans==INT_MAX?-1:ans;
     }
 };
