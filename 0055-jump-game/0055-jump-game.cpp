ed0eeec00d7076c355af916d6b142442d03a5c01
idk <145403796+itsmrx1234@users.noreply.github.com>
2023-10-24 05:07:07 +0530

    Time: 1010 ms (9.07%), Space: 51.9 MB (5.61%) - LeetHub

--- 0055-jump-game/0055-jump-game.cpp
+++ 0055-jump-game/0055-jump-game.cpp
@@ -1,25 +1,28 @@
 class Solution {
 public:
     bool generate(vector<int>& nums, int n, int curr, vector<int>& dp) {
-         if (curr >= n) {
-            return false; // Went beyond the array's bounds.
+        if (curr == n - 1) {
+            return true; // Reached the end successfully.
         }
-        if (curr == n - 1)
-            return true;
-        if (!nums[curr])
-            return false;
-        if (dp[curr] != -1)
+        if (!nums[curr]) {
+            return false; // Cannot make any progress from here.
+        }
+        if (dp[curr] != -1) {
             return dp[curr];
+        }
+
         int reach = curr + nums[curr];
         for (int i = curr + 1; i <= reach; i++) {
-            if (generate(nums, n, i, dp))
-                return dp[curr] = true;
+            if (generate(nums, n, i, dp)) {
+                return dp[curr] = true; // If a valid path is found, return true.
+            }
         }
         return dp[curr] = false;
     }
 
     bool canJump(vector<int>& nums) {
-        vector<int> dp(nums.size(), -1);
-        return generate(nums, nums.size(), 0, dp);
+        int n = nums.size();
+        vector<int> dp(n, -1);
+        return generate(nums, n, 0, dp);
     }
 };
