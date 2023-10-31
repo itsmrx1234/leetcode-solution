4b87ae76fed7dcbf5135ef57439e77d73f89344c
idk <145403796+itsmrx1234@users.noreply.github.com>
2023-10-31 19:51:46 +0530

    Time: 189 ms (95.15%), Space: 101.6 MB (84.85%) - LeetHub

---
+++ 2000-minimum-speed-to-arrive-on-time/2000-minimum-speed-to-arrive-on-time.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+   Solution() {
+        ios_base::sync_with_stdio(false);
+        cin.tie(nullptr);
+        cout.tie(nullptr);
+    }
+    int minSpeedOnTime(vector<int>& dist, double hour) {
+        double sum = 0;
+        int n = dist.size();
+        int start = 1;
+        int end = 1e7;
+        if(hour<=(double)dist.size()-1)return -1;
+        while(start<end){
+            int mid = (start+end)>>1;
+            for(int i = 0; i<n-1; i++){
+                sum+=(double)dist[i]/mid;
+                sum=ceil(sum);
+            }
+            sum+=(double)dist[n-1]/mid;
+            if(sum<=hour)end=mid;
+            else start=mid+1;
+            sum=0;
+        }
+        return end;
+    }
+};
