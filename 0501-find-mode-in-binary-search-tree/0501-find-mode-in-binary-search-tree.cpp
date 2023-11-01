29adeff73230e2ef1b9e98f5099482fcc14b8336
idk <145403796+itsmrx1234@users.noreply.github.com>
2023-11-02 01:54:49 +0530

    Time: 15 ms (69.61%), Space: 25.7 MB (45.08%) - LeetHub

--- 0501-find-mode-in-binary-search-tree/0501-find-mode-in-binary-search-tree.cpp
+++ 0501-find-mode-in-binary-search-tree/0501-find-mode-in-binary-search-tree.cpp
@@ -1,21 +1,26 @@
 class Solution {
 public:
-    unordered_map<int,int> mp;
-    vector<int>res;
-    int ans=INT_MIN;
+    Solution() {
+        ios_base::sync_with_stdio(false);
+        cin.tie(nullptr);
+        cout.tie(nullptr);
+    }
+    int cnt=0,max_ans=INT_MIN;
+    vector<int>ans;
+    unordered_map<int,int>mp;
     void helper(TreeNode* node){
         if(!node) return ;
         helper(node->left);
-        if(++mp[node->val]>ans){
-            res.clear();
-            res.push_back(node->val);
-            ans=mp[node->val];
+        if(++mp[node->val]>max_ans){
+            ans.clear();
+            ans.push_back(node->val);
+            max_ans=mp[node->val];
         }
-        else if(mp[node->val]==ans) res.push_back(node->val);
+        else if(mp[node->val]==max_ans) ans.push_back(node->val);
         helper(node->right);
     }
     vector<int> findMode(TreeNode* root) {
         helper(root);
-        return res;
+        return ans;
     }
 };
