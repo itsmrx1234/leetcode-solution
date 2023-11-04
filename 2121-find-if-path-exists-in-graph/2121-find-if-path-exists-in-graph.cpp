448c96dc6dc9f39af65f652b1d8e76fe0ad6c669
idk <145403796+itsmrx1234@users.noreply.github.com>
2023-11-04 13:50:55 +0530

    Time: 506 ms (59.1%), Space: 225.5 MB (36.99%) - LeetHub

--- 2121-find-if-path-exists-in-graph/2121-find-if-path-exists-in-graph.cpp
+++ 2121-find-if-path-exists-in-graph/2121-find-if-path-exists-in-graph.cpp
@@ -1,42 +1,27 @@
 class Solution {
 public:
-    int s, d; // source & destination
-    const static int N = 2e5 + 5;
-    bool vis[N]; // boolean array to check nodes are visited or not
-    vector<vector<int>> adj; // adjacency list
-    bool sol = 0; // solution
-
-    // dfs
-    void dfs(int u) {
-        // if node is the destination
-        if (u == d) {
-            sol = 1;
+    vector<vector<int>>adj;
+     const static int N = 2e5 + 5;
+    bool vis[N]; 
+    bool ans=false;
+    void dfs(vector<vector<int>>&adj,int start,int end){
+        if(start==end){
+            ans=true;
             return;
         }
-        // mark it as visited
-        vis[u] = 1;
-        // Recur for all the vertices adjacent
-        for (auto v : adj[u]) {
-            if (!vis[v]) {
-                dfs(v);
-            }
+        vis[start]=true;
+        for(auto it:adj[start]){
+            if(!vis[it])
+                dfs(adj,it,end);
         }
     }
-
-    bool validPath(int nn, vector<vector<int>>& edges, int source, int destination) {
-        // resize the adjacency list to the number of nodes
-        adj.resize(nn);
-
-        // fill the adjacency list
-        for (int i = 0; i < edges.size(); i++) {
-            adj[edges[i][0]].push_back(edges[i][1]);
-            adj[edges[i][1]].push_back(edges[i][0]);
+    bool validPath(int n, vector<vector<int>>& edge, int source, int destination) {
+        adj.resize(n);
+        for(int i=0;i<edge.size();i++){
+            adj[edge[i][0]].push_back(edge[i][1]);
+            adj[edge[i][1]].push_back(edge[i][0]);
         }
-
-        s = source;
-        d = destination;
-        // call dfs
-        dfs(s);
-        return sol;
+        dfs(adj,source,destination);
+        return ans;
     }
 };
