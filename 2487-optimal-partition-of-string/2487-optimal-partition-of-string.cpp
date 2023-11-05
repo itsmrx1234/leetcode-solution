4d4e702d99da482e76ebee2e94f5016c83c7e9f1
idk <145403796+itsmrx1234@users.noreply.github.com>
2023-11-06 02:38:16 +0530

    Time: 52 ms (67.36%), Space: 10.8 MB (65.9%) - LeetHub

---
+++ 2487-optimal-partition-of-string/2487-optimal-partition-of-string.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int partitionString(string nums) {
+        unordered_map<char,int>mp;
+        int start=0,end=0,n=nums.size(),cnt=0;
+        while(end<n){
+            mp[nums[end]]++;
+            if(mp[nums[end]]>1){
+                cnt++;
+                for(;start<end;start++) mp[nums[start]]--;
+            }
+            end++;
+        }
+        cout<<end<<" "<<start<<" "<<cnt;
+        return cnt+1;
+    }
+};
